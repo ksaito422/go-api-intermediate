67deed9afbdf865c45efa10a34f7fa88fec8dabb
saitooooooo <kitsit.39@gmail.com>
2022-10-07 09:08:54 +0900

    feat: テストごとの後処理

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -78,3 +78,30 @@ func TestSelectArticleDetail(t *testing.T) {
 		})
 	}
 }
+
+// InsertArticle関数のテスト
+func TestInsertArticle(t *testing.T) {
+	article := models.Article{
+		Title:    "insertTest",
+		Contents: "testest",
+		UserName: "keita",
+	}
+
+	expectedArticleNum := 3
+	newArticle, err := repositories.InsertArticle(testDB, article)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if newArticle.ID != expectedArticleNum {
+		t.Errorf("new article id is expected %d but got %d\n", expectedArticleNum, newArticle.ID)
+	}
+
+	t.Cleanup(func() {
+		const sqlStr = `
+			delete from articles
+			where title = ? and contents = ? and username = ?
+		`
+		testDB.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	})
+}
