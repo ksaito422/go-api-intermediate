cc205663e1e22b9b1aaf271071f917e8effe46d5
saitooooooo <kitsit.39@gmail.com>
2022-10-09 17:09:37 +0900

    feat: レスポンスロギングのミドルウェア実装

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,10 +5,32 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
 
-		next.ServeHTTP(w, req)
+		// 自作のResponseWriterを作って
+		rlw := NewResLoggingWriter(w)
+
+		// それをハンドラに渡す
+		next.ServeHTTP(rlw, req)
+
+		// 自作ResponseWriterからロギングしたいデータを出す
+		log.Println("res: ", rlw.code)
 	})
 }
